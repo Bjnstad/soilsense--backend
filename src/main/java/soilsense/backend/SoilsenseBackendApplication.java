a4a0f2051510097d5563caff0b9e2ed6aef9d0b6
Axel Bjørnstad <axebjo@gmail.com>
2019-04-03 22:37:16 +0200

    start structure

---
+++ src/main/java/soilsense/backend/SoilsenseBackendApplication.java
@@ -0,0 +1,13 @@
+package soilsense.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilsenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilsenseBackendApplication.class, args);
+	}
+
+}
